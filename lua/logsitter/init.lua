c9171d37d822b682e5a97c0d883e4585d222317c
Gaël Philippe <gaelphilippe@Gaels-MacBook-Pro.local>
2021-05-27 23:00:02 +0200

    chore(lint)

--- lua/logsitter/init.lua
+++ lua/logsitter/init.lua
@@ -10,90 +10,86 @@
 --
 local tsutils = require('nvim-treesitter.ts_utils')
 
-local constants = require('plugins.logsitter.constants')
-local u = require('plugins.logsitter.utils')
+local constants = require('logsitter.constants')
+local u = require('logsitter.utils')
 
 --- Finds the node after which the "log" should be inserted
 -- @todo refactor to be more legible
 local function parent_declaration(checks, node)
-  local parent = node
+    local parent = node
 
-  while parent ~= nil do
-    local type = parent:type()
-    local r = nil
-    local placement = "below"
+    while parent ~= nil do
+        local type = parent:type()
+        local r = nil
+        local placement = "below"
 
-    for _, c in ipairs(checks) do
-      if c.test(parent, type) then
-        print("found a ".. c.name)
-        r, placement = c.handle(parent, type)
+        for _, c in ipairs(checks) do
+            if c.test(parent, type) then
+                print("found a " .. c.name)
+                r, placement = c.handle(parent, type)
 
-        if r ~= nil then
-          return r, placement
+                if r ~= nil then return r, placement end
+            end
         end
-      end
-    end
 
-    parent = parent:parent()
-  end
+        parent = parent:parent()
+    end
 end
 
 -- returns the proper logger for a given format
 local function get_logger(ft)
-  return require('plugins.logsitter.lang.'..ft)
+    return require('logsitter.lang.' .. ft)
 end
 
 -- returns the posistion at which the log
 -- should be inserted
 local function get_insertion_position(logger, node, winnr)
-  local pos = vim.api.nvim_win_get_cursor(winnr)
-  local decl, placement = parent_declaration(logger.checks, node)
+    local pos = vim.api.nvim_win_get_cursor(winnr)
+    local decl, placement = parent_declaration(logger.checks, node)
 
-  local line = pos[1] or 0
-  local col = pos[2] or 0
+    local line = pos[1] or 0
+    local col = pos[2] or 0
 
-  if decl ~= nil then
-    if placement == constants.PLACEMENT_BELOW then
-      line, col = decl:end_()
-			line = line + 1
+    if decl ~= nil then
+        if placement == constants.PLACEMENT_BELOW then
+            line, col = decl:end_()
+            line = line + 1
 
-    elseif placement == constants.PLACEMENT_ABOVE then
-      line, col = decl:start()
+        elseif placement == constants.PLACEMENT_ABOVE then
+            line, col = decl:start()
 
-    elseif placement == constants.PLACEMENT_INSIDE then
-      line, col = decl:start()
+        elseif placement == constants.PLACEMENT_INSIDE then
+            line, col = decl:start()
 
-			line = line + 1
+            line = line + 1
+        end
     end
-  end
 
-  return {line, col}
+    return {line, col}
 end
 
 local M = {}
 
 function M.log(filetype)
-  local logger = get_logger(filetype)
-  if logger == nil then return end
+    local logger = get_logger(filetype)
+    if logger == nil then return end
 
-  local winnr = vim.api.nvim_get_current_win()
+    local winnr = vim.api.nvim_get_current_win()
 
-  local node = tsutils.get_node_at_cursor(winnr)
+    local node = tsutils.get_node_at_cursor(winnr)
 
-  if node == nil then
-    vim.cmd('echoerr "No node found"')
-  end
+    if node == nil then vim.cmd('echoerr "No node found"') end
 
-  local insert_pos = get_insertion_position(logger, node, winnr)
+    local insert_pos = get_insertion_position(logger, node, winnr)
 
-  local text = u.node_text(logger.expand(node))
+    local text = u.node_text(logger.expand(node))
 
-  local output = logger.log(text, insert_pos, winnr)
-  output = output .. "<esc>"
-  output = u.rtc(output)
+    local output = logger.log(text, insert_pos, winnr)
+    output = output .. "<esc>"
+    output = u.rtc(output)
 
-  vim.api.nvim_win_set_cursor(winnr, insert_pos)
-  vim.api.nvim_feedkeys(output, 'n', true)
+    vim.api.nvim_win_set_cursor(winnr, insert_pos)
+    vim.api.nvim_feedkeys(output, 'n', true)
 end
 
 return M
--- lua/logsitter/utils.lua
+++ lua/logsitter/utils.lua
@@ -3,13 +3,29 @@ local tsutils = require('nvim-treesitter.ts_utils')
 local M = {}
 
 -- Shorthand for replace_termcodes
-function M.rtc (str)
-  return vim.api.nvim_replace_termcodes(str, true, false, true)
+function M.rtc(str)
+    return vim.api.nvim_replace_termcodes(str, true, false, true)
 end
 
 -- Makes node text fit on one line
 function M.node_text(node)
-  return table.concat(tsutils.get_node_text(node), ', ')
+    return table.concat(tsutils.get_node_text(node), ', ')
+end
+
+--- Return `true` if `str` starts with `start`
+-- @string str
+-- @string start
+-- @return a boolean value
+function M.starts_with(str, start)
+    return str:sub(1, #start) == start
+end
+
+--- Return `true` if `str` ends with `start`
+-- @string str
+-- @string ending
+-- @return a boolean value
+function M.ends_with(str, ending)
+    return ending == "" or str:sub(-#ending) == ending
 end
 
 return M
